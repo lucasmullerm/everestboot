255eb39494f6cbd86645b8c96c5499011786aaa3
Lucas Muller <lucasmullerm@gmail.com>
2015-03-29 20:18:15 -0300

    deleting server folder

--- server/ISQLHandler.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-public interface ISQLHandler {
-	public void add (IHyperlink x) ;
-	public IHyperlink[] search (String s) ;
-	public IHyperlink[] tagged (String s) ;
-	public void remove (int id) ;
-}
