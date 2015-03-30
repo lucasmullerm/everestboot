058fadae423120db05ba6caa2052de038835fcfb
caiquelira <caiqueportolira@gmail.com>
2015-03-30 09:29:15 -0300

    Delete IHyperlink.java

--- IHyperlink.java
+++
@@ -1,9 +0,0 @@
-public interface IHyperlink {
-	public void setId (int id) ;
-	public int getId ();
-	public void setName (String name);
-	public String getName ();
-	public void setURL (String url) ;
-	public String getURL ();
-	public void addTag (String tag);
-}
