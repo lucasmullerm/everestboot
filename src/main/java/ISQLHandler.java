c893db642400299292eb94764f43b8e96bb30850
Caque Lira <caiqueportolira@gmail.com>
2015-03-30 09:28:06 -0300

    IHyperlink and ISQLHandler in place

---
+++ src/main/java/ISQLHandler.java
@@ -0,0 +1,6 @@
+public interface ISQLHandler {
+    public void add (IHyperlink x) ;
+    public List<IHyperlink> search (String s) ;
+    public List<IHyperlink> tagged (String s) ;
+    public void remove (int id) ;
+}
