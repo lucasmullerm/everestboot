d807c63d8ef9ba8776729317f8b59e14d0cea109
Caque Lira <caiqueportolira@gmail.com>
2015-03-30 10:32:49 -0300

    ISQLHandler is now singleton

--- src/main/java/ISQLHandler.java
+++ src/main/java/ISQLHandler.java
@@ -1,5 +1,6 @@
 public interface ISQLHandler {
     public void add (IHyperlink x) ;
+    public static ISQLHandler getInstance ();
     public List<IHyperlink> search (String s) ;
     public List<IHyperlink> tagged (String s) ;
     public void remove (int id) ;
