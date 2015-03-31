473141b4cb20e233e71be17224d3310ccd3dca3b
Caque Lira <caiqueportolira@gmail.com>
2015-03-31 09:10:53 -0300

    support to edition, comments and search by id

--- src/main/java/Hyperlink.java
+++ src/main/java/Hyperlink.java
@@ -3,12 +3,23 @@ import java.util.*;
 public class Hyperlink implements IHyperlink{
     private long id;
     private String name, url;
-    private List<String> tags;
-    public Hyperlink (long id, String name, String url, List<String> tags) {
+    private List<String> tags, comments;
+    public Hyperlink (long id, String name, String url, 
+		      List<String> tags, String comments) {
 		this.id = id;
 		this.name = name;
 		this.url = url;
 		this.tags = new ArrayList<String>(tags);
+		this.comment = comment;
+    }
+    public void setComment (String comment) {
+	this.comment = comment;
+    }
+    public String getComment () {
+	return comment;
+    }
+    public List<String> getCommentWords () {
+	return Array.asList (comment.split ("\\s+");
     }
     public void setId(long id){
 	this.id = id;
